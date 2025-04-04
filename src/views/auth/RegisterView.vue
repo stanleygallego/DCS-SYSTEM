324af6ae8b8f36fe15364cba173aac143c11ad79
Stanley <your @stanleygo1307@gmail.com>
2025-04-04 23:09:53 +0800

    feat applayout and partial theme

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,76 +1,56 @@
 <script setup>
-import { ref } from 'vue'
+import AppLayout from '@/components/layout/AppLayout.vue'
+import { useDisplay } from 'vuetify'
 
-const theme = ref('light')
-
-function onClick() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
-}
+const { mobile } = useDisplay()
 </script>
 
 <template>
-  <v-responsive class="border rounded">
-    <v-app :theme="theme">
-      <v-app-bar class="px-3" color="grey-lighten-1">
-        <v-spacer></v-spacer>
+  <AppLayout>
+    <template #content>
+      <v-row class="d-flex justify-center rounded-circle">
+        <v-col cols="12" md="6" class="pt-16">
+          <v-card class="mx-auto" elevation="24">
+            <v-card-title class="text-center">
+              <h5 class="font-weight-black">DSC Woodworks System</h5>
+              <P class="font-weight-bold">Registration Form</P>
+            </v-card-title>
 
-        <v-btn
-          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-          text="Toggle Theme"
-          slim
-          @click="onClick"
-        ></v-btn>
-      </v-app-bar>
+            <v-card-text class="bg-surface-light pt-4">
+              <v-divider class="my-5"></v-divider>
+              <v-form fast-fail @submit.prevent>
+                <v-text-field label="First Name" variant="outlined"></v-text-field>
+                <v-text-field label="Last Name" variant="outlined"></v-text-field>
+                <v-text-field label="Email" variant="outlined"></v-text-field>
 
-      <v-main>
-        <v-container>
-          <v-row class="d-flex justify-center">
-            <v-col cols="12" md="6">
-              <v-card
-                class="mx-auto"
-                prepend-icon="mdi-account-plus"
-                subtitle="Register Form"
-                elevation="24"
-              >
-                <template v-slot:title>
-                  <span class="font-weight-black">DSC Woodworks System</span>
-                </template>
+                <v-text-field label="Password" type="password" variant="outlined"></v-text-field>
 
-                <v-card-text class="bg-surface-light pt-4">
-                  <v-form fast-fail @submit.prevent>
-                    <v-text-field label="First Name" variant="outlined"></v-text-field>
-                    <v-text-field label="Last Name" variant="outlined"></v-text-field>
-                    <v-text-field label="Email" variant="outlined"></v-text-field>
+                <v-text-field
+                  label="Confirm Password"
+                  type="password"
+                  variant="outlined"
+                ></v-text-field>
 
-                    <v-text-field
-                      label="Password"
-                      type="password"
-                      variant="outlined"
-                    ></v-text-field>
+                <v-btn
+                  class="mt-2"
+                  type="submit"
+                  block
+                  color="primary"
+                  prepend-icon="mdi-account-plus"
+                  >Register</v-btn
+                >
+              </v-form>
 
-                    <v-text-field
-                      label="Confirm Password"
-                      type="password"
-                      variant="outlined"
-                    ></v-text-field>
+              <v-divider class="my-5"></v-divider>
 
-                    <v-btn class="mt-2" type="submit" block>Submit</v-btn>
-                  </v-form>
-
-                  <v-divider class="my-5"></v-divider>
-
-                  <h5 class="text-center">
-                    Already have an account?
-                    <RouterLink class="text-primary" to="/">Click here to Login</RouterLink>
-                  </h5>
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-
-      <v-footer color="grey-lighten-1" border app>Copyright 2025 - DSC Woodworks System</v-footer>
-    </v-app>
-  </v-responsive>
+              <h5 class="text-center">
+                Already have an account?
+                <RouterLink class="text-primary" to="/">Click here to Login</RouterLink>
+              </h5>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </template>
+  </AppLayout>
 </template>

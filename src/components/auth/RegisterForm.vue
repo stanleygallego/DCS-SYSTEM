ef149f6f9213860f62c0274364ec9cd4b59a4d6e
Stanley <your @stanleygo1307@gmail.com>
2025-05-01 12:32:21 +0800

    feat: added password view function

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -33,13 +33,19 @@ const isPasswordConfirmVisible = ref(false)
       </v-col>
 
       <v-col cols="12" md="6">
-        <v-text-field label="Confirm Password" type="password" variant="outlined"></v-text-field>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-btn class="mt-2" type="submit" block color="brown" prepend-icon="mdi-account-plus"
-          >Register</v-btn
-        >
+        <v-text-field
+          prepend-inner-icon="mdi mdi-lock"
+          label="Confirm Password "
+          :type="isPasswordVisible ? 'text' : 'password'"
+          :append-icon="isPasswordVisible ? 'mdi mdi-eye-off-outline' : 'mdi mdi-eye-outline'"
+          @click:append-inner="isPasswordVisible = !isPasswordVisible"
+          type="password"
+          variant="outlined"
+        ></v-text-field>
       </v-col>
     </v-row>
+    <v-btn class="mt-2" type="submit" block color="brown" prepend-icon="mdi-account-plus"
+      >Register</v-btn
+    >
   </v-form>
 </template>

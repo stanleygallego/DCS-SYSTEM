a3ec4ea0b905dc9c490d597f71da33f613dfbc5f
Stanley <your @stanleygo1307@gmail.com>
2025-05-01 16:52:45 +0800

    fix:registration errors

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -6,11 +6,8 @@ import {
   confirmedValidator,
 } from '@/utils/validators'
 import { ref } from 'vue'
-<<<<<<< HEAD
-=======
 import AlertNotification from '@/components/common/AlertNotification.vue'
-import { supabase, formActionDefault } from '@utils/supabase/js'
->>>>>>> feat/supabase
+import { supabase, formActionDefault } from '@/utils/supabase.js'
 
 const formDataDefault = {
   firstname: '',
@@ -24,15 +21,6 @@ const formData = ref({
   ...formDataDefault,
 })
 
-<<<<<<< HEAD
-const isPasswordVisible = ref(false)
-const isPasswordConfirmVisible = ref(false)
-
-const refVForm = ref()
-
-const onSubmit = () => {
-  alert(formData.value.email)
-=======
 const formAction = ref({
   ...formActionDefault,
 })
@@ -66,7 +54,6 @@ const onSubmit = async () => {
   }
 
   formAction.value.formProcess = false
->>>>>>> feat/supabase
 }
 
 const onFormSubmit = () => {
@@ -77,26 +64,9 @@ const onFormSubmit = () => {
 </script>
 
 <template>
-<<<<<<< HEAD
-  <v-form ref="refVForm" @submit.prevent="onFormSubmit">
-    <v-row>
-      <v-col cols="12" md="6">
-        <v-text-field
-          v-model="formData.firstname"
-          label="First Name"
-          :rules="[requiredValidator]"
-          variant="outlined"
-        ></v-text-field>
-      </v-col>
-
-      <v-col cols="12" md="6">
-        <v-text-field
-          v-model="formData.lastname"
-          label="Last Name"
-=======
   <AlertNotification
     :form-success-message="formAction.formSuccessMessage"
-    :form-error-message="formAction.formErrorMessage"
+    :form-error-message="formAction"
   ></AlertNotification>
 
   <v-form class="mt-5" ref="refVForm" @submit.prevent="onFormSubmit">
@@ -105,7 +75,6 @@ const onFormSubmit = () => {
         <v-text-field
           v-model="formData.firstname"
           label="First Name"
->>>>>>> feat/supabase
           :rules="[requiredValidator]"
           variant="outlined"
         ></v-text-field>
@@ -113,27 +82,18 @@ const onFormSubmit = () => {
 
       <v-col cols="12" md="6">
         <v-text-field
-<<<<<<< HEAD
-          v-model="formData.email"
-          label="Email"
-          prepend-icon="mdi-email-outline"
-          :rules="[requiredValidator, emailValidator]"
-=======
           v-model="formData.lastname"
           label="Last Name"
           :rules="[requiredValidator]"
->>>>>>> feat/supabase
           variant="outlined"
         ></v-text-field>
       </v-col>
 
       <v-col cols="12" md="6">
         <v-text-field
-<<<<<<< HEAD
-=======
           v-model="formData.email"
           label="Email"
-          prepend-icon="mdi-email-outline"
+          prepend-inner-icon="mdi-email-outline"
           :rules="[requiredValidator, emailValidator]"
           variant="outlined"
         ></v-text-field>
@@ -141,13 +101,12 @@ const onFormSubmit = () => {
 
       <v-col cols="12" md="6">
         <v-text-field
->>>>>>> feat/supabase
           v-model="formData.password"
           prepend-inner-icon="mdi mdi-lock"
           label="Password"
-          :type="isPasswordVisible ? 'text' : 'password'"
-          :append-icon="isPasswordVisible ? 'mdi mdi-eye-off-outline' : 'mdi mdi-eye-outline'"
-          @click:append-inner="isPasswordVisible = !isPasswordVisible"
+          :type="isPasswordConfirmVisible ? 'text' : 'password'"
+          :append-inner-icon="isPasswordConfirmVisible ? 'mdi-eye-off' : 'mdi-eye'"
+          @click:append-inner="isPasswordConfirmVisible = !isPasswordConfirmVisible"
           type="password"
           variant="outlined"
           :rules="[requiredValidator, passwordValidator]"
@@ -158,24 +117,19 @@ const onFormSubmit = () => {
         <v-text-field
           v-model="formData.password_confirmation"
           prepend-inner-icon="mdi mdi-lock"
-          label="Password Confirmation "
+          label="Confirm Password "
           :type="isPasswordConfirmVisible ? 'text' : 'password'"
-          :append-icon="
-            isConfirmPasswordVisible ? 'mdi mdi-eye-off-outline' : 'mdi mdi-eye-outline'
-          "
+          :append-inner-icon="isPasswordConfirmVisible ? 'mdi-eye-off' : 'mdi-eye'"
           @click:append-inner="isPasswordConfirmVisible = !isPasswordConfirmVisible"
           type="password"
           variant="outlined"
           :rules="[
             requiredValidator,
-            confirmedValidator(formDatapassword_confirmation, formData.password),
+            confirmedValidator(formData.password_confirmation, formData.password),
           ]"
         ></v-text-field>
       </v-col>
     </v-row>
-<<<<<<< HEAD
-    <v-btn class="mt-2" type="submit" block color="brown" prepend-icon="mdi-account-plus"
-=======
     <v-btn
       class="mt-2"
       type="submit"
@@ -184,7 +138,6 @@ const onFormSubmit = () => {
       prepend-icon="mdi-account-plus"
       :disabled="formAction.formProcess"
       :loading="formAction.formProcess"
->>>>>>> feat/supabase
       >Register</v-btn
     >
   </v-form>
